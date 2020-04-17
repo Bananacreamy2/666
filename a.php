deee60a3a48615d7f875d2a987f0bc85909b3d65
Bananacreamy2 <63867168+Bananacreamy2@users.noreply.github.com>
2020-04-18 00:03:23 +0700

    Update a.php

--- a.php
+++ a.php
@@ -109,7 +109,7 @@ echo "\e[89m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬�
         echo "\n".color("green","⏳▶️ Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
-        sleep(30);
+        sleep(25);
         }
         $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD160420A"}');
         $message = fetch_value($code1,'"message":"','"');
