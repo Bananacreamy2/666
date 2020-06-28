34f5db9f3533d0af2f743b65a3efd5da53ab8f9c
Bananacreamy2 <63867168+Bananacreamy2@users.noreply.github.com>
2020-06-28 22:42:45 +0700

    Update function.php

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -8.719052,115.172310";
+$header[] = "X-Location: -7.579643,110.825499";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
