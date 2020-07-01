50ad77603598264d3eae1e345f84e21e67777699
Bananacreamy2 <63867168+Bananacreamy2@users.noreply.github.com>
2020-07-01 18:58:05 +0700

    Update c.php

--- c.php
+++ c.php
@@ -112,14 +112,11 @@ echo color("green","# # # # # # # # # # # # # # # # # # # # # # # \n");
         echo color("nevy",".");
         sleep(7);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"G-XBK6LHL"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"G-D22KNJT"}');
         $message = fetch_value($code1,'"message":"','"');
-        echo "\n".color("green"," Message: ".$message);
-        echo "\n".color("yellow"," 🥂 CLAIM VOC🥂.");
-        echo "\n".color("red"," ⏳▶️Please wait");
-        for($a=1;$a<=3;$a++){
-        echo color("nevy",".");
-        sleep(7);
+        if(strpos($code1, 'Promo kamu sudah bisa dipakai.')){
+        echo "\n".color("green","Message: ".$message);
+        goto gofood;
         }else{
         echo "\n".color("red"," Message: ".$message);
         gofood:
