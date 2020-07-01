6b2d3067337e747597278706fdb567c22d26d7ff
Bananacreamy2 <63867168+Bananacreamy2@users.noreply.github.com>
2020-07-01 19:09:50 +0700

    Update c.php

--- c.php
+++ c.php
@@ -189,7 +189,7 @@ echo color("green","# # # # # # # # # # # # # # # # # # # # # # # \n");
          }
          }
          }
-         }else{
+         }
          echo color("red","-] KODE OTP LO SALAH");
          echo"\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n";
          echo color("yellow","!] INPUT ULANG NOMOR LO..\n");
